79dbc795148a3853ce5f3ac3950ac0c838e044d2
Adam Barrett <ke5gpd@gmail.com>
2015-10-16 14:46:24 -0500

    no message

--- 02 Data Wrangling/Workflow2.R
+++ 02 Data Wrangling/Workflow2.R
@@ -12,8 +12,15 @@ MainDF <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/
 #file_path <- "hygdata_v3.reformatted10k.csv"
 
 #MainDF <- read.csv(file_path, stringsAsFactors = FALSE)
+SCDF <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from STARSUBCLASS10k"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_apb766', PASS='orcl_apb766', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
 
-head(MainDF)
+for(n in names(SCDF)) {
+  SCDF[n] <- data.frame(lapply(SCDF[n], gsub, pattern="[\\( - \\)]",replacement= ""))
+  SCDF[n] <- data.frame(lapply(SCDF[n], gsub, pattern="Hash",replacement= "#"))
+}
+#head(SCDF)
+
+#head(MainDF)
 
 df <- select(MainDF, id, spect, con)
 
@@ -68,7 +75,7 @@ ggplot() +
   scale_x_discrete() +
   scale_y_discrete() +
   scale_color_manual(values=c(as.character(hexdf)))+
-  labs(title='Stars') +
+  labs(title='Color Spectrum by Constellation') +
   labs(x="Color", y=paste("Constellation")) +
   theme(axis.title.x = element_text(vjust=-0.35))+
   theme(axis.text.x=element_text(size=0))+
@@ -84,7 +91,7 @@ ggplot() +
         geom_params=list(), 
         #position=position_identity()
         position=position_jitter(width=0.5, height=0)
-  )#+
+  )
 
 
 
