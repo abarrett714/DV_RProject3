60985ef1f677478aeb9852f9ae58ff599a696cee
MEWsofCastemere <malachiw@utexas.edu>
2015-10-16 23:49:19 -0500

    no message

--- 02 Data Wrangling/Workflow3.R
+++ 02 Data Wrangling/Workflow3.R
@@ -3,13 +3,11 @@ require("RCurl")
 require("ggplot2")
 require("dplyr")
 
-NASF <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from STARSUBCLASS10k"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_mew2795', PASS='orcl_mew2795', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
-NAF <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from STARSUBCLASS10k"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_mew2795', PASS='orcl_mew2795', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
+NASF <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from SF2data"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_mew2795', PASS='orcl_mew2795', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
+NAF <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from SF2data"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_mew2795', PASS='orcl_mew2795', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
 
 dfNAF <- dplyr::anti_join(NAF, NASF, by="chi2")
 dfNAF
-
-dfNAF <- dfNAF %>% select(chi2, ID)
 
 require(extrafont)
 ggplot() + 
@@ -17,11 +15,11 @@ ggplot() +
   scale_x_discrete() +
   scale_y_continuous() +
   labs(title='Stars') +
-  labs(x="Chi squared", y=paste("ID (relative to vega)")) +
+  labs(x="Chi squared", y=paste("ID of the system")) +
   theme(axis.text.x = element_text(angle = 90)) +
   theme(legend.position="none") +
   layer(data=dfNAF, 
-        mapping=aes(as.character(chi2), y=as.numeric(as.numeric(ID)), color=chi2), 
+        mapping=aes(as.character(CHI2), y=as.numeric(as.numeric(ID)), color=CHI2), 
         stat="identity", 
         stat_params=list(), 
         geom="point",
